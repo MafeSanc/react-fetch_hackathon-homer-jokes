6e845975013ef3947c1f89004ea64c95c0c12d8c
Jenn <jenndoyle@hotmail.co.uk>
2021-02-19 10:41:05 +0000

    app

--- dadjokes/src/Components/App/index.js
+++ dadjokes/src/Components/App/index.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import JokeViewer from "../JokeViewer";
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    getJoke();
+  }, []);
+
+  async function getJoke() {
+    fetch(process.env.REACT_APP_API_URL, {
+      headers: { accept: "application/json" },
+    })
+      .then((res) => res.json())
+      .then(({ joke }) => setJoke(joke));
+  }
+
+  function handleClick() {
+    getJoke();
+  }
+
   return (
-    <div>
-      <JokeViewer />
+    <div className="App">
+      <header className="App-header">
+        <h1> The J-J-Jokes: A tribute </h1>
+        <button className="myButton" onClick={handleClick}>
+          Click to start...
+        </button>
+      
+        <p className="quote">{joke}</p>
+        <button className="myButton" onClick={handleClick}>
+          Tell me another...
+        </button>
+        <img
+          className="JJ-pic"
+          alt="JJ"
+          src="https://ca.slack-edge.com/T6L933W4X-U01J3LDNC0Z-519bc187fdf3-512"
+        />
+      </header>
     </div>
   );
 }
